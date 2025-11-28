037235f3c239a3596f51d129c5c5f2c2e8e2bc9f
Palermo Store <gagikmargaryan845@gmail.com>
2025-11-28 12:45:14 -0300

    Define main function for data loading and menu
    Added main function to load data and execute menu.

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def main():
+    habitaciones, usuarios = cargar_datos()
+    
+   
+    print(f"DEBUG: Se cargaron {len(habitaciones)} habitaciones y {len(usuarios)} usuarios.")
+   
+    
+    ejecutar_menu_principal(habitaciones, usuarios)
